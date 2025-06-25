bfb36732a809e94bad9496755df3c9e1febfcd3c
Olenic Vladimir <17513028+ovlad85@users.noreply.github.com>
2025-06-25 14:02:10 +0300

    end of section 8

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,56 +1,22 @@
 import { Box, Container, CssBaseline } from "@mui/material";
 import NavBar from "./NavBar";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
+import HomePage from "../../features/Home/HomePage";
 
 function App() {
-// const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-// const [editMode, setEditMode] = useState(false);
-
-
-
-  // const handleSelectActivity = (id: string) => {
-  //   setSelectedActivity(activities?.find(x => x.id === id));
-  //   console.log(id);
-  // }
-
-  // const handleCancelSelectActivity = () => {
-  //   setSelectedActivity(undefined);
-  // } 
-
-  // const handleOpenForm = (id?: string) => {
-  //   if (id) handleSelectActivity(id);
-  //   else handleCancelSelectActivity();
-  //   setEditMode(true);
-  // }
-
-  // const handleFormClose = () => {
-  //   setEditMode(false);
-  // }
-
-  // const handleSubmitForm = (activity: Activity) => {
-  //   // if (activity.id) {
-  //   //   setActivities(activities.map(x => x.id === activity.id ? activity : x));
-  //   // } else {
-  //   //   const newActivity = {...activity, id: activities.length.toString()};
-  //   //   setSelectedActivity(newActivity);
-  //   //   setActivities([...activities, newActivity]);
-  //   // }
-  //   console.log(activity);
-  //   setEditMode(false);
-  // }
-
-  // const handleDelete = (id: string) => {
-  //   // setActivities(activities.filter(x => x.id !== id));
-  //   console.log(id)
-  // }
+  const location = useLocation(); 
 
   return (
     <Box sx={{bgcolor:'#eeeeee', minHeight: '100vh'}}>
     <CssBaseline/>
+    {location.pathname === '/' ? <HomePage/> : (
+      <>
       <NavBar />
       <Container maxWidth="xl" sx={{mt: 3}}>
         <Outlet />
       </Container>
+      </>
+    )}
     </Box>
     
   )
